ca69d119c4929d52c7d8f163165a70cbf77823ab
Volha Patsiahevich <zaprudskaya@gmail.com>
2025-01-24 13:02:22 +0100

    feat: add more projects

--- cv/src/projects/Projects.tsx
+++ cv/src/projects/Projects.tsx
@@ -25,12 +25,42 @@ const projects = [
   {
     name: 'Image Gallery', 
     url: 'https://images.pexels.com/photos/2372977/pexels-photo-2372977.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    deploy: 'https://github.com/patciahevich/image-gallery',
+    deploy: 'https://patciahevich.github.io/image-gallery/',
   }, 
   {
     name: 'Lawyer (project in deploy)',
     url: 'https://images.pexels.com/photos/7841856/pexels-photo-7841856.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     deploy: 'https://advokatminska.by',
+  }, 
+  {
+    name: 'Battleship (WebSockets - backend part)', 
+    url: 'https://images.pexels.com/photos/18429367/pexels-photo-18429367.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    deploy: 'https://github.com/patciahevich/battleship'
+  }, 
+  {
+    name: 'Fun Chat (WebSockets - frontend part)',
+    url: 'https://images.pexels.com/photos/6669782/pexels-photo-6669782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    deploy: 'https://github.com/rolling-scopes-school/patciahevich-JSFE2023Q4/tree/fun-chat',
+  },
+  {
+    name: 'English Puzzles', 
+    url: 'https://images.pexels.com/photos/11845543/pexels-photo-11845543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    deploy: 'https://rolling-scopes-school.github.io/patciahevich-JSFE2023Q4/rss-puzzle/',
+  }, 
+  {
+    name: 'Simple CRUD API (NodeJS project)',
+    url: 'https://images.pexels.com/photos/1181772/pexels-photo-1181772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    deploy: 'https://github.com/patciahevich/CRUD_API'
+  }, 
+  {
+    name: 'File Manager (NodeJS project)',
+    url: 'https://images.pexels.com/photos/4792286/pexels-photo-4792286.jpeg',
+    deploy: 'https://github.com/patciahevich/node-js-file_manager'
+  }, 
+  {
+    name: 'Memes Slider (CSS only project)',
+    url: 'https://images.pexels.com/photos/16259439/pexels-photo-16259439.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    deploy: 'https://patciahevich.github.io/cssMemeSlider/cssMemeSlider/index.html',
   }
 ]
 
